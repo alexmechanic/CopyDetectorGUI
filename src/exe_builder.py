8c56bf6d3ff95209ffb8609ab99d2a8e1f45f6df
Alexander <samik.mechanic@gmail.com>
2022-05-16 15:45:44 +0300

    add binary builder

---
+++ src/exe_builder.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#
+# Copyright (c) 2022 Gerasimov Alexander <samik.mechanic@gmail.com>
+#
+# Executable builder
+#
+
+import PyInstaller.__main__, sys, os
+from distutils.dir_util import remove_tree
+
+# имя файла программы
+NAME = u'CopyDetectorGUI'
+
+if __name__ == '__main__':
+    print ("Building executable...", end='')
+    sys.stdout.flush()
+    PyInstaller.__main__.run([
+        '--name=%s' % NAME,
+        '--onefile',
+        '--distpath=../exe',
+        '--clean',
+        '--log-level=CRITICAL',
+        'gui.py',
+    ])
+    remove_tree("build")
+    os.remove(NAME + ".spec")
+    print("Done.\n")
