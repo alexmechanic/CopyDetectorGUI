926ea27998a7d3c34ec424f330967f8a8f3780b4
Alexander <samik.mechanic@gmail.com>
2022-05-17 19:03:48 +0300

    add venv build script

---
+++ src/make_install.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#
+# Copyright (c) 2022 Gerasimov Alexander <samik.mechanic@gmail.com>
+#
+# Executable builder main script
+#
+
+echo -n "Creating build environment..."
+python3 -m venv build_env
+echo "done."
+source ./build_env/bin/activate
+echo -n "Installing requirements..."
+python3 -m pip install -r ../requirements.txt &> /dev/null
+echo "done."
+echo -n "Building distribution..."
+python3 exe_builder.py  &> /dev/null
+echo "done."
+echo -n "Cleaning up..."
+deactivate
+# rm -rf build_env
+echo "done."
