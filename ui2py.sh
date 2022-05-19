06fedb3340c533a5e1e578d7f46c3783e2774d42
Alexander <samik.mechanic@gmail.com>
2022-05-19 16:29:15 +0300

    partial fix for 1c5d871b
    * fixed run button centering from layout

--- ui2py.sh
+++ ui2py.sh
@@ -4,6 +4,4 @@
 #
 
 pyuic5 src/ui/mainwindow.ui -o src/mainform.py
-# force-align Analyze button center of grid
-sed -i "" "s/addWidget(self.run_button, 5, 0, 1, 4)/addWidget(self.run_button, 5, 0, 1, 4, alignment=QtCore.Qt.AlignCenter)/g" src/mainform.py
 pyrcc5 src/ui/resource.rc -o src/resource.py
